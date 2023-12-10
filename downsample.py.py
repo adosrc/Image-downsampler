f7db97de0ef9e481a37c18825c840b8d417075fc
adosrc <128089313+adosrc@users.noreply.github.com>
2023-12-10 23:39:14 +0300

    Add files via upload

---
+++ downsample.py.py
@@ -0,0 +1,22 @@
+import os
+import cv2
+
+
+high_res_directory = 'hrtest/'
+low_res_directory = 'lrtest/'
+
+# Downsample High res images in output directory
+for filename in os.listdir(high_res_directory):
+
+    # Read high res images from input directory
+    high_res = cv2.imread(os.path.join(high_res_directory, filename))
+
+    # Blur images with gaussian
+    high_res = cv2.GaussianBlur(high_res, (0, 0), 1, 1)
+
+
+    # Resize 1/4
+    low_res_4x = cv2.resize(high_res, (0, 0), fx=0.5, fy=0.5,
+                           interpolation=cv2.INTER_CUBIC)
+
+    cv2.imwrite(os.path.join(low_res_directory, filename), low_res_4x)
